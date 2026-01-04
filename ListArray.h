c0bc61ca03ae61d4e5cd84a0683add05c30d6869
AngeloPeru <aorovaz@upv.edu.es>
2026-01-04 18:44:38 +0100

    Añadida implementación de la clase ListArray

--- ListArray.h
+++ ListArray.h
@@ -1,4 +1,4 @@
-#define LISTARRAY_H
+#ifndef LISTARRAY_H
 #define LISTARRAY_h
 
 #include <ostream>
@@ -12,23 +12,33 @@ class ListArray : public List<T>{
 			  int max;
 			  int n;
 			  static const int MINSIZE = 2;
+			  
 			  void resize (int new_size) {
-			  T* arr = new T[n];
-			  }
+			  	T* new_arr = new T[new_size];
+			  	for (int i = 0; i < n; ++i){
+				  new_arr[i] = arr[i];
+			  	}
+			  	delete[] arr;
+			  	arr = new_arr;
+			  	max = new_size;
+			   }
 				  
 		  
 		   public:
-		   	  int size_;
 			ListArray(){
-				T arr[MINSIZE] =} 
+				arr = new T[MINSIZE];
+				max = MINSIZE;
+				n = 0;
 			}
-			~ListArray();{
+
+			~ListArray(){
 				delete[] arr;
 			}
 
 			
-			  int size() const {
-				  return size_;
+			  int size() const override {
+				  return n;
+			  }
 			  void insert(int pos,T e) override {
 				 if (pos < 0 || pos > n){
 					throw std::out_of_range("Posicion invalida en insert()");
@@ -48,11 +58,23 @@ class ListArray : public List<T>{
 			void prepend(T e) override {
 				insert(0, e);
 			}
-			T operator[] (int pos){
-				if(pos < 0 || pos > size()-1){
+			T operator[] (int pos) override{
+				if(pos < 0 || pos >= n){
 					throw std::out_of_range("Posicion fuera del rango valido");		                   }
-				return pos;
+				return arr[pos];
 			}
 			template <typename U>
 			friend std::ostream&operator<<(std::ostream&out, const ListArray<U>&list);
 };
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const ListArray<T>& list) {
+	out << "[";
+	for (int i = 0; i < list.size(); i++) {
+		out << list.arr[i];
+		if (i < list.size() - 1) out << ", ";
+	}
+	out << "]";
+	return out;
+}
+
+#endif
